417a51708155786b24789a3d1e4bf35e05e5a8ed
Xuemin Guan <xuemin.guan@johnlewis.co.uk>
2019-05-20 16:31:17 +0100

    day 4 | Find the lowest positive integer that does not exist in the array. | more comprehensive tests

--- src/test/day4/findTheLowestPositiveIntegerThatDoesNotExistInTheArray.test.ts
+++ src/test/day4/findTheLowestPositiveIntegerThatDoesNotExistInTheArray.test.ts
@@ -7,8 +7,12 @@ describe("findTheLowestPositiveIntegerThatDoesNotExistInTheArray", () => {
         expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([])).toEqual(1);
     });
 
-    it("should give 2 for the sample data - 1", () => {
-        expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([3, 4, -1, 1])).toEqual(2);
+    it("should give 1 as the lowest int for a list which has 2 at its lowest positive int", () => {
+        expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([2])).toEqual(1);
+    });
+
+    it("should give 2 as the lowest int for a list which has 1 at its lowest positive int", () => {
+        expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([1])).toEqual(2);
     });
 
     it("should work for more complex cases", () => {
@@ -17,5 +21,17 @@ describe("findTheLowestPositiveIntegerThatDoesNotExistInTheArray", () => {
         expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([1, 2, 3, 4, 5, 6, 7])).toEqual(8);
         expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([2, 3, 4, 5, 6, 7])).toEqual(1);
         expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([2, 3, 4, 7])).toEqual(1);
+    });
+
+    it("should return 1 for list has no positive values", () => {
+        expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([-1])).toEqual(1);
+        expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([0])).toEqual(1);
+        expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([0, 0, -10, 0, -99])).toEqual(1);
+        expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([-1, -1])).toEqual(1);
+    });
+
+    it("should work for sample test data", () => {
+        expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([3, 4, -1, 1])).toEqual(2);
+        expect(findTheLowestPositiveIntegerThatDoesNotExistInTheArray([1, 2, 0])).toEqual(3);
     });
 });
